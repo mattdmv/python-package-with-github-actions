e3a0a4a87bd635c53f9182ca459448a51810f517
Matija Domovic <matija.domovic6@gmail.com>
2023-07-08 14:34:23 +0200

    add square_number function to example module

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,12 @@
-from example_package_yeerahdi.example import add_one
+import pytest
+
+from example_package_yeerahdi.example import add_one, square_number
 
 
 def test_add_one():
     assert add_one(4) == 5
+
+
+@pytest.mark.parametrize("test_input,expected", [(2, 4), (-1, 1), (0, 0)])
+def test_square_number(test_input, expected):
+    assert square_number(test_input) == expected
